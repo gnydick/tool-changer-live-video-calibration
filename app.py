af1880aabd80bb59644de1aadcda0e090b1f8cf8
Gabe E. Nydick <gnydick@nydick.net>
2023-11-21 11:14:21 -0800

    stashing working

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ bootstrap = Bootstrap5(app)
 zoom_level = 1.0
 pan_x, pan_y, old_pan_x, old_pan_y = 0, 0, 0, 0
 
-width = 1920
-height = 1080
+width = 3840
+height = 2160
 x = width / 2  # Initial pan coordinates
 y = height / 2
-pan_amount = 10
+pan_distance = 10
 x1 = old_x1 = 0
 y1 = old_y1 = 0
 x2 = old_x2 = width / 2
@@ -25,8 +25,8 @@ y2 = old_y2 = height / 2
 
 @app.route('/')
 def index():
-    global zoom_level, pan_amount
-    return render_template('index.html', zoom_level=zoom_level, pan_amount=pan_amount)
+    global zoom_level, pan_distance
+    return render_template('index.html', zoom_level=zoom_level, pan_distance=pan_distance)
 
 
 if __name__ == '__main__':
@@ -56,9 +56,10 @@ def toggle_lock():
 
 @app.route('/update-circle-params', methods=['POST'])
 def update_circle_params():
-    global circle_params
-    data = request.json
-    circle_params.update(data)
+    global lock_state, circle_params
+    if not lock_state:
+        data = request.json
+        circle_params.update(data)
     return jsonify({"message": "Circle parameters updated", "params": circle_params})
 
 
@@ -130,85 +131,89 @@ def process_frame(frame):
 
 
 def gen_frames():
-    global zoom_level, pan_amount
-    camera = cv2.VideoCapture(0)  # Use 0 for the first camera
+    global zoom_level, pan_distance
+    try:
+        camera = cv2.VideoCapture(1)  # Use 0 for the first camera
 
-    # Set the resolution of the webcam (replace WIDTH and HEIGHT with your webcam's resolution)
-    camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-    circles = None
-    old_circles = None
-    frame_counter = 0
-    generation = 0
-    while True:
-        success, frame = camera.read()
-        if not success:
-            break
-        else:
-            frame = process_frame(frame)
-            # Convert frame to grayscale
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-            # Apply Hough Circle Transform
-            if np.double(circle_params['dp']) == 0:
-                circle_params['dp'] = "0.1"
-            if np.double(circle_params['minDist']) == 0:
-                circle_params['minDist'] = "0.1"
-            if np.double(circle_params['param1']) == 0:
-                circle_params['param1'] = "0.1"
-            if np.double(circle_params['param2']) == 0:
-                circle_params['param2'] = "0.1"
-            if np.double(circle_params['minRadius']) == 0:
-                circle_params['minRadius'] = "0.1"
-            if np.double(circle_params['maxRadius']) == 0:
-                circle_params['maxRadius'] = "0.1"
-            if frame_counter % 10 == 0:
-                frame_counter = 0
-                if not lock_state:
-                    generation += 1
-
-                    circles = cv2.HoughCircles(image=gray,
-                                               method=cv2.HOUGH_GRADIENT,
-                                               dp=np.double(circle_params['dp']),
-                                               minDist=np.double(circle_params['minDist']),
-                                               param1=np.double(circle_params['param1']),
-                                               param2=np.double(circle_params['param2']),
-                                               minRadius=np.int16(circle_params['minRadius']),
-                                               maxRadius=np.int16(circle_params['maxRadius']))
-                    old_circles = circles
+        # Set the resolution of the webcam (replace WIDTH and HEIGHT with your webcam's resolution)
+        camera.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        camera.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+        circles = None
+        old_circles = None
+        frame_counter = 0
+        generation = 0
+        while True:
+            success, frame = camera.read()
+            if not success:
+                break
             else:
-                if old_circles is not None:
-                    circles = old_circles
+                frame = process_frame(frame)
+                # Convert frame to grayscale
+                gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-            # Draw circles on the frame
-            if circles is not None:
+                # Apply Hough Circle Transform
+                if np.double(circle_params['dp']) == 0:
+                    circle_params['dp'] = "0.1"
+                if np.double(circle_params['minDist']) == 0:
+                    circle_params['minDist'] = "0.1"
+                if np.double(circle_params['param1']) == 0:
+                    circle_params['param1'] = "0.1"
+                if np.double(circle_params['param2']) == 0:
+                    circle_params['param2'] = "0.1"
+                if np.double(circle_params['minRadius']) == 0:
+                    circle_params['minRadius'] = "0.1"
+                if np.double(circle_params['maxRadius']) == 0:
+                    circle_params['maxRadius'] = "0.1"
+                if frame_counter % 10 == 0:
+                    frame_counter = 0
+                    if not lock_state:
+                        generation += 1
 
-                circles_to_draw = np.round(circles[0, :]).astype("int")
-                # circles_to_draw = circles[0:1]
-                # circles_to_draw = sorted(circles, key=lambda x: x[2], reverse=True)[:2]  # Sort by radius and get top 2, don't want this
+                        circles = cv2.HoughCircles(image=gray,
+                                                   method=cv2.HOUGH_GRADIENT,
+                                                   dp=np.double(circle_params['dp']),
+                                                   minDist=np.double(circle_params['minDist']),
+                                                   param1=np.double(circle_params['param1']),
+                                                   param2=np.double(circle_params['param2']),
+                                                   minRadius=np.int16(circle_params['minRadius']),
+                                                   maxRadius=np.int16(circle_params['maxRadius']))
+                        old_circles = circles
+                else:
+                    if old_circles is not None:
+                        circles = old_circles
 
-                for i, (circleX, circleY, r) in enumerate(circles_to_draw):
-                    cv2.circle(frame, (circleX, circleY), r, (0, 255, 0), 4)
-                    text1 = f"Radius: {r}"
-                    text1Size, _ = cv2.getTextSize(text1, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 2)
-                    text1X = circleX - text1Size[0] // 2
-                    text1Y = circleY + (text1Size[1] // 2) - 10
-                    cv2.putText(frame, text1, (text1X, text1Y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-                    text2 = f"CircleID: {i}"
-                    textSize, _ = cv2.getTextSize(text2, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 2)
-                    text2X = circleX - textSize[0] // 2
-                    text2Y = (circleY + textSize[1] // 2) + 10
-                    cv2.putText(frame, text2, (text2X, text2Y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+                # Draw circles on the frame
+                if circles is not None:
 
-            frame_counter += 1
-            # Convert the frame to bytes and yield
+                    circles_to_draw = np.round(circles[0, :]).astype("int")
+                    # circles_to_draw = circles[0:1]
+                    # circles_to_draw = sorted(circles, key=lambda x: x[2], reverse=True)[:2]  # Sort by radius and get top 2, don't want this
 
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+                    for i, (circleX, circleY, r) in enumerate(circles_to_draw):
+                        cv2.circle(frame, (circleX, circleY), r, (0, 255, 0), 4)
+                        text1 = f"Radius: {r}"
+                        text1Size, _ = cv2.getTextSize(text1, cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
+                        text1X = circleX - text1Size[0] // 2
+                        text1Y = circleY + (text1Size[1] // 2) - 40
+                        cv2.putText(frame, text1, (text1X, text1Y), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 0, 0), 2)
+                        text2 = f"CircleID: {i}"
+                        textSize, _ = cv2.getTextSize(text2, cv2.FONT_HERSHEY_SIMPLEX, 2, 2)
+                        text2X = circleX - textSize[0] // 2
+                        text2Y = (circleY + textSize[1] // 2) + 40
+                        cv2.putText(frame, text2, (text2X, text2Y), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 2)
 
-    camera.release()
+                frame_counter += 1
+                # Convert the frame to bytes and yield
+
+                ret, buffer = cv2.imencode('.jpg', frame)
+                frame = buffer.tobytes()
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        camera.release()
+    except:
+        pass
+
+
 
 
 @app.route('/video_feed')
@@ -218,24 +223,25 @@ def video_feed():
 
 @app.route('/pan', methods=['POST'])
 def handle_pan():
-    global pan_x, pan_y, zoom_level, old_pan_x, old_pan_y
-    data = request.json
-    pan_amount = int(data["pan_amount"])
-    direction = data["direction"]
+    global pan_x, pan_y, zoom_level, old_pan_x, old_pan_y, lock_state
+    if not lock_state:
+        data = request.json
+        pan_distance = int(data["pan_distance"])
+        direction = data["direction"]
 
-    if direction == 'left':
-        old_pan_x = pan_x
-        pan_x -= pan_amount
+        if direction == 'left':
+            old_pan_x = pan_x
+            pan_x -= pan_distance
 
-    elif direction == 'right':
-        old_pan_x = pan_x
-        pan_x += pan_amount
-    elif direction == 'up':
-        old_pan_y = pan_y
-        pan_y -= pan_amount
-    elif direction == 'down':
-        old_pan_y = pan_y
-        pan_y += pan_amount
+        elif direction == 'right':
+            old_pan_x = pan_x
+            pan_x += pan_distance
+        elif direction == 'up':
+            old_pan_y = pan_y
+            pan_y -= pan_distance
+        elif direction == 'down':
+            old_pan_y = pan_y
+            pan_y += pan_distance
 
     # You might want to add boundary checks here to prevent pan_x and pan_y from going out of bounds
 
@@ -244,9 +250,10 @@ def handle_pan():
 
 @app.route('/zoom', methods=['POST'])
 def zoom():
-    global zoom_level
-    data = request.json
-    zoom_level = float(data["zoom_level"])
+    global zoom_level, lock_state
+    if not lock_state:
+        data = request.json
+        zoom_level = float(data["zoom_level"])
 
     # You might want to add boundary checks here to prevent pan_x and pan_y from going out of bounds
 
---
+++ static/js/stuff.js
@@ -0,0 +1,302 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+
+      function jog(direction) {
+        // Implement the logic to handle jog control
+        console.log('Jogging', direction);
+    }
+
+
+    function mySubmitFunction() {
+
+    }
+
+
+    function setupParameterSlider(minInputId, sliderId, maxInputId, valueId) {
+        var minInput = document.getElementById(minInputId);
+        var slider = document.getElementById(sliderId);
+        var maxInput = document.getElementById(maxInputId);
+        var valueDisplay = document.getElementById(valueId);
+
+        function updateValueDisplay() {
+            valueDisplay.textContent = slider.value;
+        }
+
+        function updateSliderLimits() {
+            slider.min = minInput.value;
+            slider.max = maxInput.value;
+            slider.value = Math.min(slider.value, slider.max);
+            updateValueDisplay();
+        }
+
+        minInput.addEventListener('change', updateSliderLimits);
+        maxInput.addEventListener('change', updateSliderLimits);
+        slider.addEventListener('input', updateValueDisplay);
+
+        updateSliderLimits()
+
+        function sendSliderData() {
+            var formData = {
+                dp: document.getElementById('dp-slider').value,
+                minDist: document.getElementById('minDist-slider').value,
+                param1: document.getElementById('param1-slider').value,
+                param2: document.getElementById('param2-slider').value,
+                minRadius: document.getElementById('minRadius-slider').value,
+                maxRadius: document.getElementById('maxRadius-slider').value,
+                tool: getSelectedTool('dynamic-tools').value
+            };
+
+            fetch('/update-circle-params', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formData)
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log('Success:', data);
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+        }
+
+        slider.oninput = function () {
+            updateValueDisplay();
+            sendSliderData(); // Send data when the slider value changes
+        };
+
+        minInput.onchange = sendSliderData;
+        maxInput.onchange = sendSliderData;
+
+        updateValueDisplay(); // Initialize at setup
+    }
+
+// Initialize sliders for each parameter
+    setupParameterSlider('dp-min', 'dp-slider', 'dp-max', 'dp-value');
+    setupParameterSlider('minDist-min', 'minDist-slider', 'minDist-max', 'minDist-value');
+    setupParameterSlider('param1-min', 'param1-slider', 'param1-max', 'param1-value');
+    setupParameterSlider('param2-min', 'param2-slider', 'param2-max', 'param2-value');
+    setupParameterSlider('minRadius-min', 'minRadius-slider', 'minRadius-max', 'minRadius-value');
+    setupParameterSlider('maxRadius-min', 'maxRadius-slider', 'maxRadius-max', 'maxRadius-value');
+
+
+    function getSelectedTool(containerId) {
+        var container = document.getElementById(containerId);
+        return container.querySelector('.selected');
+    }
+
+    document.getElementById('circle-detection-form').addEventListener('change', function (event) {
+        var formData = {
+            dp: document.getElementById('dp-slider').value,
+            minDist: document.getElementById('minDist-slider').value,
+            param1: document.getElementById('param1-slider').value,
+            param2: document.getElementById('param2-slider').value,
+            minRadius: document.getElementById('minRadius-slider').value,
+            maxRadius: document.getElementById('maxRadius-slider').value,
+            tool: getSelectedTool('dynamic-tools').value
+        };
+
+        fetch('/update-circle-params', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData)
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    });
+
+    function initialResize() {
+        document.getElementById('camera-feed').style.width = '50%';
+        createTools();
+    }
+
+    //  selection box instead of buttons
+    // let isDragging = false;
+    // let isResizing = false;
+    // let dragStartX, dragStartY;
+    // const selectionSquare = document.getElementById('selection-square');
+    // const resizeHandle = document.getElementById('resize-handle');
+    // const cameraFeed = document.getElementById('camera-feed');
+    // const image = document.getElementById('camera-feed-img');
+    //
+    //
+    //
+    // selectionSquare.addEventListener('mousedown', function(e) {
+    //     if (e.target === resizeHandle) {
+    //         isResizing = true;
+    //         e.preventDefault(); // Prevent default drag behavior when resizing
+    //     } else {
+    //         isDragging = true;
+    //         dragStartX = e.clientX - selectionSquare.offsetLeft;
+    //         dragStartY = e.clientY - selectionSquare.offsetTop;
+    //     }
+    //     cameraFeed.addEventListener('mousemove', onMouseMove);
+    //     cameraFeed.addEventListener('mousemove', onResize);
+    // });
+    //
+    // document.addEventListener('mouseup', function() {
+    //     isDragging = false;
+    //     isResizing = false;
+    //     cameraFeed.removeEventListener('mousemove', onMouseMove);
+    //     cameraFeed.removeEventListener('mousemove', onResize);
+    // });
+    //
+    // function onMouseMove(e) {
+    //     if (!isDragging) return;
+    //     const rect = image.getBoundingClientRect();
+    //     const maxX = rect.width + rect.left - selectionSquare.offsetWidth;
+    //     const maxY = rect.height + rect.top - selectionSquare.offsetHeight;
+    //
+    //     let x = e.clientX - dragStartX;
+    //     let y = e.clientY - dragStartY;
+    //
+    //     x = Math.min(Math.max(rect.left, x), maxX) - rect.left;
+    //     y = Math.min(Math.max(rect.top, y), maxY) - rect.top;
+    //
+    //     selectionSquare.style.left = `${x}px`;
+    //     selectionSquare.style.top = `${y}px`;
+    // }
+    //
+    // function onResize(e) {
+    //     if (!isResizing) return;
+    //     const rect = image.getBoundingClientRect();
+    //     const maxX = e.clientX - selectionSquare.offsetLeft;
+    //     const maxY = e.clientY - selectionSquare.offsetTop;
+    //
+    //     let width = Math.min(maxX, rect.width);
+    //     let height = Math.min(maxY, rect.height);
+    //
+    //     selectionSquare.style.width = `${width}px`;
+    //     selectionSquare.style.height = `${height}px`;
+    // }
+
+
+});
+
+
+function pan(direction) {
+    const panDistance = document.getElementById('pan-distance').value;
+    var formData = {
+        direction: direction,
+        pan_distance: panDistance
+    };
+    // Example: Send the pan direction and distance to the server
+    fetch('/pan', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+
+function zoom() {
+    const zoomLevel = document.getElementById('zoom-slider').value;
+    var valueDisplay = document.getElementById("zoom-value");
+    valueDisplay.textContent = zoomLevel;
+    var formData = {
+        "zoom_level": zoomLevel
+    };
+
+    fetch('/zoom', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+
+function selectTool(index) {
+    const tools = document.getElementById('dynamic-tools').children;
+    for (let i = 0; i < tools.length; i++) {
+        if (i === index) {
+            tools[i].classList.add('selected');
+        } else {
+            tools[i].classList.remove('selected');
+        }
+    }
+}
+
+function createTools() {
+    var selected = document.querySelectorAll('button.tool.selected');
+    const count = Math.max(1, document.getElementById('toolCount').value); // Ensure at least 1
+    const container = document.getElementById('dynamic-tools');
+    container.innerHTML = ''; // Clear existing buttons
+
+
+    for (let i = 0; i < count; i++) {
+        const button = document.createElement('button');
+        button.id = i;
+        button.className = "tool";
+        if (selected.length === 1) {
+            if (selected[0].id === i.toString()) {
+                button.classList.add('selected');
+            }
+        }
+        if (count === 1) {
+            button.classList.add('selected');
+        }
+        button.setAttribute("value", i.toString())
+        button.innerText = `Tool ${i}`;
+        button.onclick = function () {
+            selectTool(i);
+        };
+        container.appendChild(button);
+    }
+}
+
+function initialResize() {
+    document.getElementById('camera-feed').style.width = '100%';
+    createTools();
+}
+
+function resizeVideo() {
+    var slider = document.getElementById('size-slider').addEventListener('input', function () {
+        var video = document.getElementById('camera-feed'); // Ensure this id matches your video element
+        var w = video.style.width
+        var sliderValue = this.value;
+        video.style.width = sliderValue + '%';
+        video.style.height = 'auto';
+    });
+}
+
+function lock() {
+
+    // Example: Send the pan direction and distance to the server
+    fetch('/toggle-lock', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+
+    }).then(response => response.json()).then(data => {
+        var button = document.getElementById('lockButton')
+        button.value = data['locked_state'];
+        console.log('Success:', data);
+    }).catch((error) => {
+        console.error('Error:', error);
+    });
+}
